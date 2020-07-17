ca2784ccf1fc13778d9d0f9b796ae50a6d61e291
Mdidu <alexandre.meddas@gmail.com>
2020-07-17 11:26:51 +0200

    add product

--- src/Controller/ECommerceController.php
+++ src/Controller/ECommerceController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ECommerceController extends AbstractController
@@ -43,4 +49,54 @@ class ECommerceController extends AbstractController
             'name' => $name
         ]);
     }
+
+    /**
+     * @Route("/{name}/new", name="create_product")
+     */
+    public function createProduct($name,
+                                Request $request,
+                                EntityManagerInterface $manager)
+    {
+        $product = new Product();
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        $categorys = $repository->findAll();
+
+        $form = $this->createFormBuilder($product)
+            ->add('title')
+            ->add('description')
+            ->add('image', TextType::class, ['required' => false])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('price', NumberType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTime());
+
+            $manager->persist($product);
+            $manager->flush();
+
+            return $this->redirectToRoute('list_product', 
+            ['id' => $product->getCategory()]);
+        }
+
+        return $this->render('e_commerce/createProduct.html.twig', [
+            'formProduct' => $form->createView(),
+            'categorys' => $categorys
+        ]);
+    }
+    /**
+     * afficher les produits dans la liste
+     * créer page produit
+     * design global du site
+     * search bar
+     * home page
+     * les logs
+     * panier
+     * commande
+     */
 }
