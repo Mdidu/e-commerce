c7655637bdbf5f155d9146912235f0032dbe72a3
Mdidu <alexandre.meddas@gmail.com>
2020-08-20 13:41:03 +0200

    update password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Ranking;
+use App\Form\NewPasswordType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -48,6 +49,58 @@ class SecurityController extends AbstractController
     public function login() {        
         return $this->render('logs/login.html.twig');
     }
+
+    /**
+     * @Route("/reset-password", priority=10, name="reset_password")
+     */
+    public function reset(Request $request, \Swift_Mailer $mailer) {
+
+        $message = (new \Swift_Message('Mail de récupération'))
+            ->setFrom('alexandre.meddas@outlook.fr')
+            ->setTo($request->get('email'))
+            ->setBody(
+                $this->renderView('emails/reset.html.twig',
+                ['email' => $request->get('email')]),
+                'text/html'
+            )
+        ;
+        $mailer->send($message);
+
+        $this->addFlash('message', 'Un mail vous à été envoyé !');
+        
+        return $this->render('logs/reset.html.twig');
+    }
+    /**
+     * @Route("/new-password/{email}", priority=10, name="update_password")
+     */
+    public function updatePassword($email,
+                                   Request $request,
+                                   EntityManagerInterface $manager,
+                                   UserPasswordEncoderInterface $encoder) {
+
+        $userDB = $this->getDoctrine()->getRepository(User::class)->findOneByEmail($email);
+
+        $form = $this->createForm(NewPasswordType::class, $userDB);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($userDB, $userDB->getPassword());
+
+            $userDB->setPassword($hash);
+
+            $manager->persist($userDB);
+            $manager->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('logs/newPassword.html.twig',
+        [
+            'form' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/logout", priority=10, name="logout")
      */
